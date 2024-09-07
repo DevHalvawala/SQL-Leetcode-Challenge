76ceacef51c79699aba987644905d3be211d451e
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-07 10:56:24 +0530

    Day-10

---
+++ Avg_Time_of_Process_per_Machine.sql
@@ -0,0 +1,45 @@
+-- SOLUTION --
+
+
+
+-- QUESTION --
+THERE IS A FACTORY WEBSITE THAT HAS SEVERAL MACHINES EACH RUNNING THE SAME NUMBER OF PROCESSES. WRITE A SOLUTION TO FIND THE AVERAGE TIME EACH MACHINE TAKES TO COMPLETE A PROCESS.
+THE TIME TO COMPLETE A PROCESS IS THE 'end' TIMESTAMP MINUS THE 'start' TIMESTAMP. THE AVERAGE TIME IS CALCULATED BY THE TOTAL TIME TO COMPLETE EVERY PROCESS ON THE MACHINE DIVIDED BY THE NUMBER OF PROCESSES THAT WERE RUN.
+THE RESULTING TABLE SHOULD HAVE THE MACHINE_ID ALONG WITH THE AVERAGE TIME AS PROCESSING_TIME, WHICH SHOULD BE ROUNDED TO 3 DECIMAL PLACES.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Activity table:
++------------+------------+---------------+-----------+
+| machine_id | process_id | activity_type | timestamp |
++------------+------------+---------------+-----------+
+| 0          | 0          | start         | 0.712     |
+| 0          | 0          | end           | 1.520     |
+| 0          | 1          | start         | 3.140     |
+| 0          | 1          | end           | 4.120     |
+| 1          | 0          | start         | 0.550     |
+| 1          | 0          | end           | 1.550     |
+| 1          | 1          | start         | 0.430     |
+| 1          | 1          | end           | 1.420     |
+| 2          | 0          | start         | 4.100     |
+| 2          | 0          | end           | 4.512     |
+| 2          | 1          | start         | 2.500     |
+| 2          | 1          | end           | 5.000     |
++------------+------------+---------------+-----------+
+Output: 
++------------+-----------------+
+| machine_id | processing_time |
++------------+-----------------+
+| 0          | 0.894           |
+| 1          | 0.995           |
+| 2          | 1.456           |
++------------+-----------------+
+Explanation: 
+There are 3 machines running 2 processes each.
+Machine 0's average time is ((1.520 - 0.712) + (4.120 - 3.140)) / 2 = 0.894
+Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
+Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
