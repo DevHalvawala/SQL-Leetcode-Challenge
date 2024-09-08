e8a22ed2242a5ac2fdb557e57cc20b4d583dbfa2
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-08 12:15:48 +0530

    Day-11

---
+++ Employee_bonus.sql
@@ -0,0 +1,40 @@
+-- SOLUTION --
+
+
+
+
+
+-- QUESTION --
+
+WRITE A SOLUTION TO REPORT THE NAME AND BONUS AMOUNT OF EACH EMPLOYEE WITH A BONUS LESS THAN 1000.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
