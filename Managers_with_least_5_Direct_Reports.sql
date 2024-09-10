51fdc3cc9a8e552b685dc63522c6c9aff29040a3
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-10 11:44:56 +0530

    Day-13

---
+++ Managers_with_least_5_Direct_Reports.sql
@@ -0,0 +1,31 @@
+-- SOLUTION --
+
+
+
+-- QUESTION --
+
+WRITE A SOLUTION TO FIND MANAGERS WITH AT LEAST FIVE DIRECT REPORTS.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
