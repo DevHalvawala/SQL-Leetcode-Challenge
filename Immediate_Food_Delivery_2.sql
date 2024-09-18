bff6c4b00f841637fab6b96594b4ca88195eef10
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-18 13:24:00 +0530

    Day-21

--- Immediate_Food_Delivery_2.sql
+++ Immediate_Food_Delivery_2.sql
@@ -1,5 +1,22 @@
 -- SOLUTION --
 
+
+SELECT
+    ROUND(AVG(ORDER_DATE = CUSTOMER_PREF_DELIVERY_DATE)*100, 2) AS IMMEDIATE_PERCENTAGE
+FROM
+    DELIVERY
+WHERE
+    (CUSTOMER_ID, ORDER_DATE) IN(
+        SELECT
+            CUSTOMER_ID,
+            MIN(ORDER_DATE)
+        FROM
+            DELIVERY
+        GROUP BY
+            CUSTOMER_ID
+    )
+
+
 -- QUESTION --
 
 
