d0d679573b9aacc5ea5c95a169698625001f8d41
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-20 11:43:55 +0530

    Day-23

---
+++ Number_of_Unique_Subjects_Taught_by_Teacher.sql
@@ -0,0 +1,38 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO CALCULATE THE NUMBER OF UNIQUE SUBJECTS EACH TEACHER TEACHES IN THE UNIVERSITY.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS SHOWN IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+Explanation: 
+Teacher 1:
+  - They teach subject 2 in departments 3 and 4.
+  - They teach subject 3 in department 3.
+Teacher 2:
+  - They teach subject 1 in department 1.
+  - They teach subject 2 in department 1.
+  - They teach subject 3 in department 1.
+  - They teach subject 4 in department 1.
