bc98729492c5362c8bad2f4449fb3ce865e616c0
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-21 11:24:51 +0530

    Day-24

---
+++ User_Activity_past_30_days.sql
@@ -0,0 +1,34 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO FIND THE DAILY ACTIVE USER COUNT FOR A PERIOD OF 30 DAYS ENDING 2019-07-27 INCLUSIVELY. A USER WAS ACTIVE ON SOMEDAY IF THEY MADE AT LEAST ONE ACTIVITY ON THAT DAY.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output: 
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+ 
+Explanation: Note that we do not care about days with zero active users.
