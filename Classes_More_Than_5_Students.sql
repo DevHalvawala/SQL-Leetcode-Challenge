c819e40498aab11f774dd588e2b16138d3c122c0
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-23 12:24:52 +0530

    Day-26

---
+++ Classes_More_Than_5_Students.sql
@@ -0,0 +1,35 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO FIND ALL THE CLASSES THAT HAVE AT LEAST FIVE STUDENTS.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
