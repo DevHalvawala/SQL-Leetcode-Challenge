abf0e68544b4821d62336c752532e85711e8065f
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-25 17:53:01 +0530

    Day-28

---
+++ Biggest_Single_Number.sql
@@ -0,0 +1,34 @@
+-- SOLUTION --
+
+-- QUESTION --
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+FIND THE LARGEST SINGLE NUMBER. IF THERE IS NO SINGLE NUMBER, REPORT NULL.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 3   |
+| 3   |
+| 1   |
+| 4   |
+| 5   |
+| 6   |
++-----+
+Output: 
++-----+
+| num |
++-----+
+| 6   |
++-----+
+Explanation: The single numbers are 1, 4, 5, and 6.
+Since 6 is the largest single number, we return it.
