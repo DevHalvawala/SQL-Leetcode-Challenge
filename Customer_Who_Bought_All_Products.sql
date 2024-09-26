4e5e29c5910743d1c3e71dc822027daea14e3648
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-26 12:59:24 +0530

    Day-29

---
+++ Customer_Who_Bought_All_Products.sql
@@ -0,0 +1,36 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO REPORT THE CUSTOMER IDS FROM THE CUSTOMER TABLE THAT BOUGHT ALL THE PRODUCTS IN THE PRODUCT TABLE.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Customer table:
++-------------+-------------+
+| customer_id | product_key |
++-------------+-------------+
+| 1           | 5           |
+| 2           | 6           |
+| 3           | 5           |
+| 3           | 6           |
+| 1           | 6           |
++-------------+-------------+
+Product table:
++-------------+
+| product_key |
++-------------+
+| 5           |
+| 6           |
++-------------+
+Output: 
++-------------+
+| customer_id |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: 
+The customers who bought all the products (5 and 6) are customers with IDs 1 and 3.
