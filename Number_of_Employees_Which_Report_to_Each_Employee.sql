0c8fee16c7a4dd20f2b9dad00be289aa74135bbf
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-27 11:00:21 +0530

    Day-30

---
+++ Number_of_Employees_Which_Report_to_Each_Employee.sql
@@ -0,0 +1,26 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO REPORT THE IDS AND THE NAMES OF ALL MANAGERS, THE NUMBER OF EMPLOYEES WHO REPORT DIRECTLY TO THEM, AND THE AVERAGE AGE OF THE REPORTS ROUNDED TO THE NEAREST INTEGER.
+RETURN THE RESULT TABLE ORDERED BY EMPLOYEE_ID.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Employees table:
++-------------+---------+------------+-----+
+| employee_id | name    | reports_to | age |
++-------------+---------+------------+-----+
+| 9           | Hercy   | null       | 43  |
+| 6           | Alice   | 9          | 41  |
+| 4           | Bob     | 9          | 36  |
+| 2           | Winston | null       | 37  |
++-------------+---------+------------+-----+
+Output: 
++-------------+-------+---------------+-------------+
+| employee_id | name  | reports_count | average_age |
++-------------+-------+---------------+-------------+
+| 9           | Hercy | 2             | 39          |
++-------------+-------+---------------+-------------+
+Explanation: Hercy has 2 people report directly to him, Alice and Bob. Their average age is (41+36)/2 = 38.5, which is 39 after rounding it to the nearest integer.
