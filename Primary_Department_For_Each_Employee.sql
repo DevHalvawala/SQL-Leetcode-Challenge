9c402078eedd99d5692d887954e150b0ac691a73
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-28 11:16:57 +0530

    Day-30

---
+++ Primary_Department_For_Each_Employee.sql
@@ -0,0 +1,36 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO REPORT ALL THE EMPLOYEES WITH THEIR PRIMARY DEPARTMENT. FOR EMPLOYEES WHO BELONG TO ONE DEPARTMENT, REPORT THEIR ONLY DEPARTMENT.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Employee table:
++-------------+---------------+--------------+
+| employee_id | department_id | primary_flag |
++-------------+---------------+--------------+
+| 1           | 1             | N            |
+| 2           | 1             | Y            |
+| 2           | 2             | N            |
+| 3           | 3             | N            |
+| 4           | 2             | N            |
+| 4           | 3             | Y            |
+| 4           | 4             | N            |
++-------------+---------------+--------------+
+Output: 
++-------------+---------------+
+| employee_id | department_id |
++-------------+---------------+
+| 1           | 1             |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
++-------------+---------------+
+Explanation: 
+- The Primary department for employee 1 is 1.
+- The Primary department for employee 2 is 1.
+- The Primary department for employee 3 is 3.
+- The Primary department for employee 4 is 3.
