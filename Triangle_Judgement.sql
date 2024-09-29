5e58738a23736cb2e482db9c48b799a68c0cca25
Dev Halvawala <devhalvawala15@gmail.com>
2024-09-29 11:18:26 +0530

    Day-32

---
+++ Triangle_Judgement.sql
@@ -0,0 +1,27 @@
+-- SOLUTION --
+
+
+-- QUESTION --
+
+REPORT FOR EVERY THREE LINE SEGMENTS WHETHER THEY CAN FORM A TRIANGLE.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
