56e0cc14c283e43721a6c46007bbb165b8f8bc4e
Dev Halvawala <devhalvawala15@gmail.com>
2024-10-01 14:21:14 +0530

    Day-34

---
+++ Product_Price_at_Given_Date.sql
@@ -0,0 +1,29 @@
+-- QUESTION --
+
+WRITE A SOLUTION TO FIND THE PRICES OF ALL PRODUCTS ON 2019-08-16. ASSUME THE PRICE OF ALL PRODUCTS BEFORE ANY CHANGE IS 10.
+RETURN THE RESULT TABLE IN ANY ORDER.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output: 
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
