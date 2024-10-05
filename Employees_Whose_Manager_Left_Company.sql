a2cc5c767ad48f42f71ecb35cfaa37865c60a11f
Dev Halvawala <devhalvawala15@gmail.com>
2024-10-05 11:16:40 +0530

    Day-35

--- Employees_Whose_Manager_Left_Company.sql
+++ Employees_Whose_Manager_Left_Company.sql
@@ -1,3 +1,21 @@
+-- SOLUTION --
+
+SELECT
+    EMPLOYEE_ID
+FROM
+    EMPLOYEES
+WHERE
+    SALARY < 30000
+    AND MANAGER_ID NOT IN (
+        SELECT
+            EMPLOYEE_ID
+        FROM
+            EMPLOYEES
+    )
+ORDER BY
+    EMPLOYEE_ID;
+
+    
 -- QUESTION --
 
 Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
