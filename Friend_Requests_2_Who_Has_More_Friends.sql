6da9275cdd4899a8333183d2371f9cf907a6cb8b
Dev Halvawala <devhalvawala15@gmail.com>
2024-10-09 11:25:14 +0530

    Day-39

---
+++ Friend_Requests_2_Who_Has_More_Friends.sql
@@ -0,0 +1,29 @@
+-- SOLUTION --
+-- QUESTION --
+
+WRITE A SOLUTION TO FIND THE PEOPLE WHO HAVE THE MOST FRIENDS AND THE MOST FRIENDS NUMBER.
+THE TEST CASES ARE GENERATED SO THAT ONLY ONE PERSON HAS THE MOST FRIENDS.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+RequestAccepted table:
++--------------+-------------+-------------+
+| requester_id | accepter_id | accept_date |
++--------------+-------------+-------------+
+| 1            | 2           | 2016/06/03  |
+| 1            | 3           | 2016/06/08  |
+| 2            | 3           | 2016/06/08  |
+| 3            | 4           | 2016/06/09  |
++--------------+-------------+-------------+
+Output: 
++----+-----+
+| id | num |
++----+-----+
+| 3  | 3   |
++----+-----+
+Explanation: 
+The person with id 3 is a friend of people 1, 2, and 4, so he has three friends in total, 
+which is the most number than any others.
