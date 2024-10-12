89404a1c1a2801e4d3d9f375d886f354508a419b
Dev Halvawala <devhalvawala15@gmail.com>
2024-10-12 14:52:45 +0530

    Day-42

---
+++ Fix_Names_in_Table.sql
@@ -0,0 +1,27 @@
+-- SOLUTION --
+
+
+-- QUESTION --
+
+WRITE A SOLUTION TO FIX THE NAMES SO THAT ONLY THE FIRST CHARACTER IS UPPERCASE AND THE REST ARE LOWERCASE.
+RETURN THE RESULT TABLE ORDERED BY USER_ID.
+THE RESULT FORMAT IS IN THE FOLLOWING EXAMPLE.
+EXAMPLE 1:
+
+
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
